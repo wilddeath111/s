740a5a42610c4989fb7a17ce647ef27a9d6b2532
wilddeath111 <140193775+wilddeath111@users.noreply.github.com>
2025-02-15 16:56:48 +0300

    Add files via upload

---
+++ Language/en/Site.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Files language strings.
+ *
+ * @package      CodeIgniter
+ * @author       CodeIgniter Dev Team
+ * @copyright    2014-2019 British Columbia Institute of Technology (https://bcit.ca/)
+ * @license      https://opensource.org/licenses/MIT    MIT License
+ * @link         https://codeigniter.com
+ * @since        Version 3.0.0
+ * @filesource
+ * 
+ * @codeCoverageIgnore
+ */
+return [
+    'back_to_top_text' => 'Back to top',
+    'about_text' => 'About',
+    'contact_text' => 'Contact',
+    'follow_twitter_text' => 'Follow on Twitter',
+    'like_facebook_text' => 'Like on Facebook',
+    'blog' => [
+        'related_posts_text' => 'Related Posts',
+        'views_text' => 'views',
+        'faq_text' => 'Frequently Asked Questions',
+        'min_read_text' => 'mins read',
+    ],
+
+    'error_text' => 'HATA! : ',
+    'error_try_again_text' => 'Bir hata oluştu, tekrar deneyin!',
+    'reCAPTCHA_novalidate_text' => 'reCAPTCHA doğrulama başarısız oldu.',
+    'comment_success_text' => 'Yorumunuz başarıyla kaydedildi.',
+
+    'comments' => [
+        'title_text' => 'Yorumlar',
+        'write_title_text' => 'Yorum Yaz',
+        'name_text' => 'İsim soyisim',
+        'email_text' => 'E-posta Adresiniz',
+        'comment_text' => 'Yorumunuz',
+        'submit_button_text' => 'Yorum Yap'
+
+    ]
+];
